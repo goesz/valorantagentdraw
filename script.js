15b7cdb650871b0840fb5f3d54ae3e1cacebeb8a
Rafael Goes <goesdeveloper@gmail.com>
2023-11-13 14:55:29 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+let numAgentes = document.getElementById("players").value
+let agentes = ["Brimstone", "Viper", "Omen", "Raze", "Cypher", "Sova", "Sage", "Phoenix", "Jett", "Breach", "Killjoy", "Reyna", "Skye", "Yoru", "Astra", "KAY/O", "Deadlock", "Iso", "Gekko", "Chamber", "Neon", "Fade", "Harbor"]
+
+function verificarNum(numAgentes) {
+  if (!/[0-9]/.test(numAgentes)) {
+    return false;
+  }
+  return true;
+}
+
+    function sortAgent() {
+      let numJogadores = document.getElementById("players").value;
+
+      if (verificarNum(numJogadores)) {
+        if (numJogadores > agentes.length) {
+          document.getElementById("escrever").innerHTML = "Não há agentes suficientes para o número de jogadores que você digitou.";
+          return;
+        }
+
+        let agentesSorteados = [];
+
+        for (let i = 0; i < numJogadores; i++) {
+          let indiceSorteado = Math.floor(Math.random() * agentes.length);
+          let agenteSorteado = agentes.splice(indiceSorteado, 1)[0];
+          agentesSorteados.push(agenteSorteado);
+        }
+
+        document.getElementById("escrever").innerHTML = `Agentes Sorteados:<br> <b>${agentesSorteados.join(`,<br> `)}.</b>`;
+      } else {
+        document.getElementById("escrever").innerHTML = "Digite um número válido de jogadores.";
+      }
+    }
